3ff5a15efbe94dcaeb490b7be375dc78cf575293
NicoTognella <nicolastognella@gmail.com>
2021-08-18 21:34:45 +1000

    css arreglado

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -11,12 +11,13 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 let productsController = {
 
-    
+    //funciona
     index: function (req, res) {
         return res.render  ('../views/productIndex', {products})
         
     },
 
+    //funciona
     detail: function (req, res) {
 
         const product = products.find( elem => elem.id == req.params.id);
@@ -35,10 +36,13 @@ let productsController = {
         return res.render (path.resolve('./views/productCreate'))
     },
 
+
     // creo una constante que usa en la variable products (mi BD) el metodo find, para encontrar una elemento de la array segundo la condicion que le doy en base al id
     // el metodo find recibe un callback que va a funcionar sobre cada elemento de la array
     // la condicion que le doy al callback es que el element (es el nombre que le asigno a la interacion o repeticion de la array), primero va con el elemento con id 1 dps 2 etc
     
+
+    //funciona
     edit: function (req, res) {
         let productEditar = products.find(element => element.id == req.params.id)
         return res.render  ('../views/productEdit', {productEditar})
